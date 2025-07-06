2672f5e3cf1bb8303608b55e269bf2935e3afc54
Srinivas Sadula <sadula.srinivas0000@gmail.com>
2025-07-06 21:51:01 +0530

    1.0.3 || Add: Home page (landing page added)

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
-
+    stats = [
+    { title: 'Total Users', value: '5,200+', delta: '+3.1%' },
+    { title: 'Resumes Built', value: '20,000+', delta: '+12.6%' },
+    { title: 'Downloads', value: '12,500+', delta: '+5.3%' },
+    { title: 'Avg Rating', value: '4.8 ★', delta: 'from 1,420 users' }
+  ];
+start(){
+  console.log('start')
+}
 }
